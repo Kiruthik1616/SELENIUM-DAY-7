f39f8eb6f033ef51ca893a4c67c52e0df75ba2f0
Kiruthik1616 <100832723+Kiruthik1616@users.noreply.github.com>
2023-05-03 09:22:29 +0530

    Add files via upload

---
+++ PRO1/src/test/java/day7/Sample.java
@@ -0,0 +1,21 @@
+package day7;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+  @Test
+  public void openGoogle() {
+	  WebDriverManager.chromedriver().setup();
+      ChromeOptions co=new ChromeOptions();
+      co.addArguments("--remote-allow-origins=*");
+      WebDriver driver=new ChromeDriver(co);
+      driver.get("https://www.google.com");
+  }
+}
---
+++ PRO1/src/test/java/day7/task2.java
@@ -0,0 +1,46 @@
+package day7;
+
+
+
+//import java.util.Scanner;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class task2 {
+	int a,b,c;
+	@BeforeSuite
+	void getdata(){
+//		System.out.println("Enter the values to calculate:");
+//		Scanner sc=new Scanner(System.in);
+//		a=sc.nextInt();
+//		b=sc.nextInt();
+//		c=sc.nextInt();
+		a=7;
+		b=4;
+	}
+@Test(priority=4)
+public void add() {
+	  c=a+b;
+	  Assert.assertEquals(c,11);
+}
+
+@Test(priority=1)
+public void sub() {
+	  c=a-b;
+	  Assert.assertEquals(c, 3);
+}
+
+@Test(priority=3)
+public void mul() {
+	  c=a*b;
+	  Assert.assertEquals(c, 28);
+}
+
+@Test(priority=2)
+public void div() {
+	  c=a/b;
+	  Assert.assertEquals(c, 1);
+}
+}
